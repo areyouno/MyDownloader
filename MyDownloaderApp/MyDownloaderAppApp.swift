cee1b977065e81da0053d77ee51437400451b6a2
areyouno <rrocsalev@gmail.com>
2023-05-10 23:00:55 +0800

    Initial Commit

---
+++ MyDownloaderApp/MyDownloaderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDownloaderAppApp.swift
+//  MyDownloaderApp
+//
+//  Created by Rreuno Velasco on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MyDownloaderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
